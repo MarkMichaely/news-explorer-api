c17586f874b43ffe68005d963569d2233aefc849
mark <markmichaely@gmail.com>
2023-04-18 17:06:17 +0300

    created schema for user and article

---
+++ models/article.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { urlRegex } = require('../utils/regex');
+
+const articleSchema = mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegex.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL`,
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegex.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+    select: false,
+  },
+});
